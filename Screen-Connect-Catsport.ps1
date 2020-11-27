a78261beea8d362c05c0be3e08acef1dda892a97
Underbee <67017660+Underbee@users.noreply.github.com>
2020-11-27 08:43:34 -0500

    Update Screen-Connect-Catsport.ps1

--- Screen-Connect-Catsport.ps1
+++ Screen-Connect-Catsport.ps1
@@ -1,13 +1,18 @@
+
 <# Install Automate#>
 $DownloadPath = "https://screenconnect.aidepc.ca:4443/Bin/One-Sky.ClientSetup.exe?h=screenconnect.aidepc.ca&p=8041&k=BgIAAACkAABSU0ExAAgAAAEAAQDVaolrsK5KPjWzlyXPqf2Tgu54EBdrxl5sr55VJawQru7JFBNcp1ZwyDia2xnaBRsUimo%2BQ3O78dLnqa94ADRnD1hUr%2FBkUaKUd9%2ByU0ZwIqnS8je%2Br7YEia9MdWrPyukD3wM5zy7D67%2BTH8a%2Bi%2BUyVtR2DQaWGGoIg9bnlR6r2gvGUElchkHx7VCKgjnys8CvTmmrfrcYOTFxGtnfmIb11x800zxWeotLMcpH9Mpo5l17GbqVAjyovs%2FusQyvYyTYyXjleFeIymOooos%2B8q4VYe2XyVs%2F9xWUA48dQAn1LqZ5C%2BNGX%2BT4vC3CNSw7BUGw%2FHHrudTMSiMZzySFK%2Fa5&e=Access&y=Guest&t=&c=Catsports&c=&c=&c=&c=&c=&c=&c="
 $SoftwarePath = "C:\Temp\One-SkySCClient.exe"
-
+if (-not (Test-Path -LiteralPath $DirectoryToCreate)) {
+    mkdir "C:\temp"
+} Try {
     Write-Host "Downloading from: $($DownloadPath)"
     Write-Host "Downloading to:   $($SoftwarePath)"
         $WebClient = New-Object System.Net.WebClient
         $WebClient.DownloadFile($DownloadPath, $SoftwarePath)
     Write-Host "Download Complete"
-
+} catch {
+    Write-Host "Error in creating temp Folder! Error: " $process.ExitCode
+    }
 $InstallExitCode = (Start-Process -FilePath $SoftwarePath -ArgumentList " /s /CopyLogs %windir%\temp\Win10Update.log" -Wait -Verb RunAs -PassThru)
 If ($InstallExitCode -eq 0) {
     If (!$Silent) {Write-Verbose "The ScreenConnect Installer Executed Without Errors"}
